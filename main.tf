6200d03b52fa6b132e964ea9951d5f4abacb517b
Saboor <rafiq.saboor@gmail.com>
2026-07-04 17:31:56 -0400

    Test: reintroduce public ACL exposure to verify pipeline catches regression

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "demo" {
 
 resource "aws_s3_bucket_public_access_block" "demo" {
   bucket                  = aws_s3_bucket.demo.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
